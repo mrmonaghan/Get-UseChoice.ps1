c8956f031c7d5bf24290661a7eac38250cb7e155
mrmonaghan <54335905+mrmonaghan@users.noreply.github.com>
2019-08-21 14:10:56 -0600

    Add files via upload

---
+++ Get-UserChoice.ps1
@@ -0,0 +1,40 @@
+﻿function Get-UserChoice {
+[CmdletBinding()]
+
+param(
+    [Parameter(
+        Mandatory = $True,
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $True)]
+    [Alias('Name','Input')]
+    [Object]$Data,
+
+    [Parameter()]
+    [string]$Title,
+
+    [Parameter()]
+    [string]$Description
+
+)
+
+    Begin {
+        $DefaultChoiceIndex = -1
+        $Options = @()
+        $i = 0
+        }
+    Process {
+        foreach ($value in $Data) {
+            $i++
+            $NewChoice = [System.Management.Automation.Host.ChoiceDescription]::new("&$value")
+            $NewChoice | Add-Member -MemberType NoteProperty -Name Name -Value $value
+            $Options += $NewChoice
+            }
+        }
+
+
+    end {
+        $ChosenIndex = $host.UI.PromptForChoice($Title,$Description,$Options,$DefaultChoiceIndex)
+        $Chosen = $Options[$ChosenIndex].name
+        Write-Output $Chosen
+        }
+}
